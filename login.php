42d1012eeb58dd628930e6aa7713fc55cf0ee38e
Danilo Popović <90621288+popovicd99@users.noreply.github.com>
2023-04-15 13:38:12 +0200

    Stine izmene u izgledu indexa

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ session_start();
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Foundation | Welcome</title>
+  <title>Music library</title>
   <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css" />
   <link rel="stylesheet" href="css/index.css" />
   <link rel="stylesheet" href="css/login.css" />
